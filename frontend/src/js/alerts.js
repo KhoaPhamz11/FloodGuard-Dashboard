53f95aecc37927893e9a9a9b568bc94edde0b36a
KhoaPham <khoapham080806@gmail.com>
2026-09-09 22:32:40 +0700

    Change unit to cm

--- frontend/src/js/alerts.js
+++ frontend/src/js/alerts.js
@@ -298,7 +298,7 @@ function renderActiveAlerts() {
                     <div class="ac-level">${alert.title}</div>
                     <div class="ac-title">Trạm: ${districtName}</div>
                     <div class="ac-summary-grid">
-                        <div>Mực nước: ${alert.waterLevel} m</div>
+                        <div>Mực nước: ${alert.waterLevel} cm</div>
                         
                     </div>
                 </div>
